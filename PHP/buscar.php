d2390ac88148f42cf1de24e3107f6f6fa34d7c07
tawanrio <stawanrio@gmail.com>
2021-11-10 17:46:16 -0300

    estudo 10/11

--- PHP/buscar.php
+++ PHP/buscar.php
@@ -1,7 +1,5 @@
 <?php
-include ('conexao2.php');
-
-
+include ('conexao.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,14 +7,14 @@ include ('conexao2.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buscar Produts</title>
+    <title>Buscar Produtos</title>
 </head>
 <body>
     
 <h1>Buscar Produto</h1>
-<form action="">
-    <input type="text" placeholder="Escreva aqui sua pesquisa"  method="post" name="busca" >
-    <input type="submit" value="Pesquisar">
+<form action="" >
+    <input type="text" placeholder="Escreva aqui sua pesquisa" method="post"  name="busca" >
+    <input type="submit" name="SendPesqUser" value="Pesquisar">
 </form>
 
 <table border="1" width="550">
@@ -25,20 +23,38 @@ include ('conexao2.php');
         <th>Descricao</th>
         <th>Preco</th>
     </tr>
+   
     <tr>
         <td colspan="3">Digite algo para Pesquisar</td>
     </tr>
 
+     
 </table>
 
 <?php
 
-$pesquisa = $mysqli->real_escape_string($_GET['busca']);
-$sql_code = "SELECT * FROM produtos WHERE descricao LIKE '$pesquisa' OR codigoL '$pesquisa' ";
+
+$pesquisa = $_GET['busca'];
+
+$consulta = "SELECT * FROM produtos WHERE descricao LIKE '%$pesquisa%'";
+
+$con = $mysqli->query($consulta) or die($mysqli->error);
+
+
+while($dados= mysqli_fetch_assoc($con)){
+    echo "<tr>".$dados['codigoL']."</tr><br><br>";
+    echo "<tr>".$dados['descricao']."</tr><br><br>";
+    echo "<tr>".$dados['preco']."</tr><br>";
+}
 
 
 
-$sql_query = $mysqli->query($sql_code) or die("ERRO ao consultar! " . $mysqli->error);
+
+// $sql_code = "SELECT * FROM produtos WHERE descricao LIKE '$pesquisa' OR codigoL '$pesquisa' ";
+
+
+
+//$sql_query = $mysqli->query($sql_code) or die("ERRO ao consultar! " . $mysqli->error);
 
 
 
--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,11 +1,24 @@
 <?php 
+/*
 define("SERVIDOR","localhost");
 define("USER", "root");
 define("SENHA", "");
 define("BANCO", "tintasestancia");
+*/
+$usuario = 'root';
+$senha = '';
+$database = "tintasestancia";
+$host="localhost";
 
-$banco = new mysqli(SERVIDOR,USER,SENHA,BANCO);
+$mysqli = new mysqli($host,$usuario,$senha,$database);
 
+//$mysqli = new mysqli(SERVIDOR,USER,SENHA,BANCO);
+
+if ($mysqli->error){
+    die("falha ao conectar o banco:".$mysqli->error);
+}
+    
+/*
 function inserirDados($codigoL,$descricao,$unidade,$preco){
     $banco = new mysqli(SERVIDOR,USER,SENHA,BANCO);
     $sql = "INSERT INTO produtos (codigoL,descricao,unidade,preco)"
@@ -15,6 +28,6 @@ function inserirDados($codigoL,$descricao,$unidade,$preco){
     $banco->close();
 
 }
-
+*/
 
 ?>
