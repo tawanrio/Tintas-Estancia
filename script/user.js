1c7731b3c4a61da34907d3ebe0812016ac60ce88
tawanrio <stawanrio@gmail.com>
2022-01-19 00:38:33 -0300

    19/01

--- script/user.js
+++ script/user.js
@@ -3,7 +3,7 @@ const login = `
     <input required class="col-12 mb-1" type="text" name="login" placeholder="Login"> 
     <input required class="col-12" type="password" name="pass" placeholder="Senha">
     <input class="formNavSubmit col-7 mt-1 btn btn-outline-dark" type="submit" id="btnConsultarUsuario" value="Entrar">
-    <a class="row justify-content-center " href="">Esqueceu a senha?</a>
+    <a class="row justify-content-center " href="#">Esqueceu a senha?</a>
 </form>`;
 function telaNavBar(){
     criaElementoAll('div','.offcanvas-body','login','login botao','login',login);
